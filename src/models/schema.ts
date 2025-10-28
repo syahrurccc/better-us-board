d7a94e51e4a4b294d0492087aa4f202d693e444c
kizuna <syahrur.ramadhan94@gmail.com>
2025-10-28 16:59:30 +0800

    minor corrections

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,17 +1,26 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true},
-  partnerId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User', default: null }
-});
-
 const boardSchema = new Schema({
   name: { type: String, default: 'Our Board' },
   userIds: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'User' }],
 }, { timestamps: true })
-  
+
+const commentSchema = new Schema({
+  ticketId: { type: mongoose.SchemaTypes.ObjectId, ref: 'Ticket', index: true },
+  authorId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
+  body: String,
+}, { timestamps: true });
+
+const inviteSchema = new Schema({
+  inviterId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
+  inviteeEmail: { type: String, required: true },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted'],
+    default: 'open'
+  },
+}, { timestamps: true });
+
 const ticketSchema = new Schema({
   boardId: { type: mongoose.SchemaTypes.ObjectId, ref: 'Board', index: true },
   authorId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
@@ -34,14 +43,15 @@ const ticketSchema = new Schema({
   },
 }, { timestamps: true });
 
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
+  partnerId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User', default: null }
+});
 
-const commentSchema = new Schema({
-  ticketId: { type: mongoose.SchemaTypes.ObjectId, ref: 'Ticket', index: true },
-  authorId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
-  body: String,
-}, { timestamps: true });
-
-export const User = mongoose.model('User', userSchema);
 export const Board = mongoose.model('Board', boardSchema);
-export const Ticket = mongoose.model('Ticket', ticketSchema);
 export const Comment = mongoose.model('Comment', commentSchema);
+export const Invite = mongoose.model('Invite', inviteSchema)
+export const Ticket = mongoose.model('Ticket', ticketSchema);
+export const User = mongoose.model('User', userSchema);
--- src/routes/connections.ts
+++ src/routes/connections.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
+import { z } from 'zod';
+
+import { User, Invite } from '../models/schema';
 
 const router = Router();
 
+const inviteSchema = z.object({
+  inviteeId: z.string().min(24).max(24)
+})
+
 router.post('/invite', async (req, res, next) => {
+  const parsedData = inviteSchema.safeParse(req.body);
   
+  if (!parsedData.success) return res.status(400).json({ error: 'Invalid userId.' });
+  
+  const invitee = await User.findById(`${parsedData.data.inviteeId}`).lean();
+  
+  if (!invitee) return res.status(400).json({ error: 'No user found.' });
+  
+  await Invite.create({
+    inviterId: req.userId,
+    inviteeEmail: invitee.email,
+  });
 });
 
 router.post('/accept', async (req, res, next) => {
